12744311833356357df620525c8049b0442da9be
unknown <anderson.lima18pb@gmail.com>
2023-09-25 20:20:28 -0300

    exercicio1

--- LS-2023/AVALIAÇÃO1.js
+++ LS-2023/AVALIAÇÃO1.js
@@ -1,46 +1,42 @@
+const readline = require('readline');
+
 class Exam {
-    constructor(respostasCorretas, pesoQuestao) {
-      this.respostasCorretas = respostasCorretas;
-      this.pesoQuestao = pesoQuestao;
-    }
-  
-    calcularNota(respostasAluno) {
-      if (respostasAluno.length !== this.respostasCorretas.length) {
-        return "Número de respostas incorreto. Por favor, responda a todas as questões.";
-      }
-  
-      let nota = 0;
-      for (let i = 0; i < respostasAluno.length; i++) {
-        if (respostasAluno[i].toUpperCase() === this.respostasCorretas[i]) {
-          nota += this.pesoQuestao;
-        }
-      }
-  
-      return nota;
-    }
+  constructor(respostasCorretas, pesoQuestao) {
+    this.respostasCorretas = respostasCorretas;
+    this.pesoQuestao = pesoQuestao;
   }
-  
-  function obterNotaDoAluno() {
-    const respostasCorretas = ["A", "B", "C", "D", "E"];
-    const pesoQuestao = 2;
-  
-    const exam = new Exam(respostasCorretas, pesoQuestao);
-  
-    const respostasAluno = [];
-  
-    for (let i = 1; i <= 5; i++) {
-      const resposta = prompt(`Digite a resposta da questão ${i}:`);
-      respostasAluno.push(resposta.toUpperCase());
+
+  calcularNota(respostasAluno) {
+    if (respostasAluno.length !== this.respostasCorretas.length) {
+      return "Número de respostas incorreto. Por favor, responda a todas as questões.";
     }
-  
-    const nota = exam.calcularNota(respostasAluno);
-  
-    if (typeof nota === "number") {
-      alert(`A nota do aluno é: ${nota}`);
-    } else {
-      alert(nota);
+
+    let nota = 0;
+    for (let i = 0; i < respostasAluno.length; i++) {
+      if (respostasAluno[i].toUpperCase() === this.respostasCorretas[i]) {
+        nota += this.pesoQuestao;
+      }
     }
+
+    return nota;
   }
-  
-  // Chamar a função para iniciar o programa
-  obterNotaDoAluno();
+}
+
+function obterNotaDoAluno() {
+  const respostasCorretas = ["A", "B", "A", "C", "D"];
+  const pesoQuestao = 2;
+  const respostasAluno = ["A", "B", "B", "B", "B"];
+
+  const exam = new Exam(respostasCorretas, pesoQuestao);
+
+  const nota = exam.calcularNota(respostasAluno);
+
+  if (typeof nota === "number") {
+    console.log(`A nota do aluno é: ${nota}`);
+  } else {
+    console.log(nota);
+  }
+}
+
+// Chamar a função para iniciar o programa
+obterNotaDoAluno();
